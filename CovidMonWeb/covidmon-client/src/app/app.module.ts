3ca30f517ab26422b44395a46973f4ffa5119f39
Anton Petrov <anton.petrov.m@gmail.com>
2023-07-11 14:31:24 +0500

    USmap has been removed because it doesn't work yet

--- CovidMonWeb/covidmon-client/src/app/app.module.ts
+++ CovidMonWeb/covidmon-client/src/app/app.module.ts
@@ -5,12 +5,11 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { MetricSelectorComponent } from './metric-selector/metric-selector.component';
-import { UnitedstatesMapComponent } from './unitedstates-map/unitedstates-map.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UnitedstatesMapComponent,
+    
     
     
   ],
